7721dc77be9c930dadd607f7da412595ff8fe831
unknown <abdullahishuaibumaje.com>
2023-09-16 14:44:27 -0700

    set up completed

--- YtWordCountProject/YoutubeTranscriptApp/views.py
+++ YtWordCountProject/YoutubeTranscriptApp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
 
 # Create your views here.
 
 def wordTranscriptCount(request):
+    if request.method=="POST":
+        video_url=request.POST['yt_url']
+        query=request.POST['query']
+        video_id=video_url[::-2]
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        word_count="word_count obtained after the checking the json object "
+        return render(request, 'home.html', {'word_count':word_count})
+    else:
+        return render(request, 'home.html')
+        
     pass
 
